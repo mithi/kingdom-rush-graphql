a51193aa4d3151eb51a52ec5a8bf380d3dc7f099
Mithi <mithi.sevilla@gmail.com>
2020-10-14 21:39:55 +0800

    test: Add preliminary tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    clearMocks: true,
+    maxWorkers: 1,
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/test/*.[jt]s?(x)"],
+}
---
+++ test/tower_db.test.ts
@@ -0,0 +1,30 @@
+import { createConnection, getConnection, getRepository } from "typeorm"
+import { TowerType, TowerLevel, TowerKingdom, Tower } from "../src/entity/Tower"
+
+beforeAll(async () => {
+    await createConnection("test")
+})
+
+afterAll(async () => {
+    await getConnection("test").close()
+})
+
+test("Store tower and fetch it", async () => {
+    const tower = {
+        name: "Militia Barracks",
+        towerType: TowerType.BARRACKS,
+        level: TowerLevel.LVL1,
+        kingdom: TowerKingdom.KR,
+        notes:
+            " This tower spawns three militia which block and attack enemies. Militia are the most basic soldier available, merely civilians with very little training, armor, and weapons. However, with additional tower support, they can hold their own against even a much larger army. They can fall pretty quickly to Orcs, so it appears that they aren't as tough as the tower description says.",
+    }
+
+    await getRepository(Tower, "test").insert(tower)
+    let retrievedTowers = await getRepository(Tower, "test").find({
+        where: {
+            name: "Militia Barracks",
+        },
+    })
+
+    expect(retrievedTowers[0].name).toBe("Militia Barracks")
+})
