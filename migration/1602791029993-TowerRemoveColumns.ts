74df553ce325fcdffa2732c1e1b34ed290508ac9
Mithi <mithi.sevilla@gmail.com>
2020-10-16 04:07:29 +0800

    chore: Setup migrations

---
+++ migration/1602791029993-TowerRemoveColumns.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class RemoveSomeColumnsInTower1602790120279 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "Towers" DROP COLUMN "notes"`)
+        await queryRunner.query(`ALTER TABLE "Towers" DROP COLUMN "towerCategory"`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "Towers" ADD COLUMN "notes"`)
+        await queryRunner.query(`ALTER TABLE "Towers" ADD COLUMN "towerCategory"`)
+    }
+}
