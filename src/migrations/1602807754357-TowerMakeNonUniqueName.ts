8e004ff9294fa0bbb6895851f77e58bfa9a388ea
Mithi <mithi.sevilla@gmail.com>
2020-10-17 00:58:22 +0800

    Move migrations to src
    - I am unable to build the app otherwise
    
    (“Cannot use import statement outside a module” in typeorm migration)

--- src/migrations/1602807754357-TowerMakeNonUniqueName.ts
+++ src/migrations/1602807754357-TowerMakeNonUniqueName.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class MakeNonUniqueName1602807754357 implements MigrationInterface {
+export class TowerMakeNonUniqueName1602807754357 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `ALTER TABLE "Towers" DROP CONSTRAINT "UQ_70fea5a9b9cbd8b66dc86b35df2"`
--- src/migrations/1602811161119-TowerMakePairUnique.ts
+++ src/migrations/1602811161119-TowerMakePairUnique.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class makePairUnique1602811161119 implements MigrationInterface {
+export class TowerMakePairUnique1602811161119 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `ALTER TABLE "Towers" ADD CONSTRAINT unique_tower UNIQUE (name, kingdom)`
