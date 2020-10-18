a6644be566897fd34bea794eba91668319f88b2b
Mithi <mithi.sevilla@gmail.com>
2020-10-18 22:55:23 +0800

    Update test to count distinct tower ids

--- test/tower_db.test.ts
+++ test/tower_db.test.ts
@@ -64,10 +64,38 @@ test("Store a tower and add main stats", async () => {
 
 test("Seed() should populate distinct rows in the tables: Towers, main_stats, barack_stats", async () => {
     await seed({ dbName: "test", verbose: false })
-    let towerCount = await getRepository(Tower, "test").count()
-    let mainStatsCount = await getRepository(MainStats, "test").count()
-    let barracksStatsCount = await getRepository(BarracksStats, "test").count()
-    let attackStatsCount = await getRepository(AttackStats, "test").count()
+
+    const towerCount = Number(
+        (
+            await getRepository(Tower, "test").query(
+                `SELECT COUNT(*) FROM (SELECT DISTINCT name, kingdom FROM "Towers") as kingdom`
+            )
+        )[0].count
+    )
+
+    const mainStatsCount = Number(
+        (
+            await getRepository(MainStats, "test").query(
+                `SELECT COUNT(DISTINCT "towerId") FROM main_stats`
+            )
+        )[0].count
+    )
+
+    const barracksStatsCount = Number(
+        (
+            await getRepository(BarracksStats, "test").query(
+                `SELECT COUNT(DISTINCT "towerId") FROM barracks_stats`
+            )
+        )[0].count
+    )
+
+    const attackStatsCount = Number(
+        (
+            await getRepository(AttackStats, "test").query(
+                `SELECT COUNT(DISTINCT "towerId") FROM attack_stats`
+            )
+        )[0].count
+    )
 
     expect(towerCount).toBe(104)
     expect(mainStatsCount).toBe(104)
@@ -77,13 +105,8 @@ test("Seed() should populate distinct rows in the tables: Towers, main_stats, ba
     // reseeding shouldn't change the rows
     // because the table wouldn't allow for duplication
     await seed({ dbName: "test", verbose: false })
-    towerCount = await getRepository(Tower, "test").count()
-    mainStatsCount = await getRepository(MainStats, "test").count()
-    barracksStatsCount = await getRepository(BarracksStats, "test").count()
-    attackStatsCount = await getRepository(AttackStats, "test").count()
-
-    expect(towerCount).toBe(104)
-    expect(mainStatsCount).toBe(104)
-    expect(barracksStatsCount).toBe(27)
-    expect(attackStatsCount).toBe(77)
+    expect(towerCount).toBe(await getRepository(Tower, "test").count())
+    expect(mainStatsCount).toBe(await getRepository(MainStats, "test").count())
+    expect(barracksStatsCount).toBe(await getRepository(BarracksStats, "test").count())
+    expect(attackStatsCount).toBe(await getRepository(AttackStats, "test").count())
 })
