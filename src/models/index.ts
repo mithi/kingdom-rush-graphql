fb8cb60b8f2a4b0b0c87c7c005f9ef8550542b03
Mithi <mithi.sevilla@gmail.com>
2020-10-20 15:19:00 +0800

    Add test for inserting in abilities (etc) tables

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,5 +2,7 @@ import { AttackStats } from "./AttackStats"
 import { MainStats } from "./MainStats"
 import { BarracksStats } from "./BarracksStats"
 import { Tower } from "./Tower"
+import { Ability } from "./Ability"
+import { AbilityLevel } from "./AbilityLevel"
 
-export { AttackStats, MainStats, BarracksStats, Tower }
+export { AttackStats, MainStats, BarracksStats, Tower, Ability, AbilityLevel }
