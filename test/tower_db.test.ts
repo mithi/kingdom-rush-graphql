f08a58ec782d82c76d63ccbfa02f35d08e7c9fa7
Mithi <mithi.sevilla@gmail.com>
2020-10-21 04:38:10 +0800

    Refactor tests

--- test/tower_db.test.ts
+++ test/tower_db.test.ts
@@ -19,6 +19,8 @@ afterAll(async () => {
 })
 
 test("Store tower and fetch it", async () => {
+    const TOWER_REPO = getRepository(Tower, "test")
+
     const tower = {
         name: "Militia Barracks",
         towerType: TowerType.BARRACKS,
@@ -26,19 +28,22 @@ test("Store tower and fetch it", async () => {
         kingdom: TowerKingdom.KR,
     }
 
-    await getRepository(Tower, "test").insert(tower)
-    let retrievedTowers = await getRepository(Tower, "test").find({
+    await TOWER_REPO.insert(tower)
+    let retrievedTowers = await TOWER_REPO.find({
         where: {
             name: "Militia Barracks",
         },
     })
 
     expect(retrievedTowers[0].name).toBe("Militia Barracks")
-    await getRepository(Tower, "test").remove(retrievedTowers[0])
-    expect(await getRepository(Tower, "test").count()).toBe(0)
+    await TOWER_REPO.remove(retrievedTowers[0])
+    expect(await TOWER_REPO.count()).toBe(0)
 })
 
 test("Store a tower and add main stats, deleting the tower would also delete main stats", async () => {
+    const TOWER_REPO = getRepository(Tower, "test")
+    const MAIN_STATS_REPO = getRepository(MainStats, "test")
+
     let tower = new Tower()
     tower.name = "dwarven bombard"
     tower.kingdom = TowerKingdom.KR
@@ -51,10 +56,9 @@ test("Store a tower and add main stats, deleting the tower would also delete mai
     mainStats.damageMaximum = 15
     tower.mainStats = mainStats
 
-    const repo = getRepository(Tower, "test")
-    await repo.save(tower)
+    await TOWER_REPO.save(tower)
 
-    let retrievedTowers = await repo.find({
+    let retrievedTowers = await TOWER_REPO.find({
         where: {
             name: "dwarven bombard",
         },
@@ -64,12 +68,16 @@ test("Store a tower and add main stats, deleting the tower would also delete mai
     expect(retrievedTowers[0].name).toBe("dwarven bombard")
     expect(retrievedTowers[0].mainStats.buildCost).toBe(125)
 
-    await repo.remove(retrievedTowers[0])
-    expect(await getRepository(Tower, "test").count()).toBe(0)
-    expect(await getRepository(MainStats, "test").count()).toBe(0)
+    await TOWER_REPO.remove(retrievedTowers[0])
+    expect(await TOWER_REPO.count()).toBe(0)
+    expect(await MAIN_STATS_REPO.count()).toBe(0)
 })
 
 test("Be able to store abilities and ability levels of a tower", async () => {
+    const TOWER_REPO = getRepository(Tower, "test")
+    const ABILITY_REPO = getRepository(Ability, "test")
+    const ABILITY_LEVEL_REPO = getRepository(AbilityLevel, "test")
+
     let tower = new Tower()
     tower.name = "dwarven bombard"
     tower.kingdom = TowerKingdom.KR
@@ -88,20 +96,20 @@ test("Be able to store abilities and ability levels of a tower", async () => {
     ability1.levels = [ability1level1]
 
     tower.abilities = [ability1]
-    await getRepository(Tower, "test").save(tower)
+    await TOWER_REPO.save(tower)
 
-    expect(await getRepository(Tower, "test").count()).toBe(1)
-    expect(await getRepository(Ability, "test").count()).toBe(1)
-    expect(await getRepository(AbilityLevel, "test").count()).toBe(1)
+    expect(await TOWER_REPO.count()).toBe(1)
+    expect(await ABILITY_REPO.count()).toBe(1)
+    expect(await ABILITY_LEVEL_REPO.count()).toBe(1)
 
-    let retrievedTowers = await getRepository(Tower, "test").find({
+    let retrievedTowers = await TOWER_REPO.find({
         where: {
             name: "dwarven bombard",
         },
         relations: ["abilities"],
     })
 
-    let abilities = await getRepository(Ability, "test").find({
+    let abilities = await ABILITY_REPO.find({
         where: {
             name: retrievedTowers[0].abilities[0].name,
         },
@@ -112,46 +120,39 @@ test("Be able to store abilities and ability levels of a tower", async () => {
     expect(retrievedTowers[0].abilities[0].name).toBe("poison arrows")
     expect(abilities[0].levels[0].cost).toBe(250)
 
-    await getRepository(Tower, "test").remove(retrievedTowers[0])
-    expect(await getRepository(Tower, "test").count()).toBe(0)
-    expect(await getRepository(Ability, "test").count()).toBe(0)
-    expect(await getRepository(AbilityLevel, "test").count()).toBe(0)
+    await TOWER_REPO.remove(retrievedTowers[0])
+    expect(await TOWER_REPO.count()).toBe(0)
+    expect(await ABILITY_REPO.count()).toBe(0)
+    expect(await ABILITY_LEVEL_REPO.count()).toBe(0)
 })
 
-test("Seed() should populate distinct rows in the tables: Towers, main_stats, barack_stats", async () => {
+test("Seed() should populate distinct rows in the tables: Towers, main_stats, barracks_stats", async () => {
+    const TOWER_REPO = getRepository(Tower, "test")
+    const MAIN_STATS_REPO = getRepository(MainStats, "test")
+    const BARRACKS_STATS_REPO = getRepository(BarracksStats, "test")
+    const ATTACK_STATS_REPO = getRepository(AttackStats, "test")
+
     await seed({ dbName: "test", verbose: false })
 
-    const towerCount = Number(
-        (
-            await getRepository(Tower, "test").query(
-                `SELECT COUNT(*) FROM (SELECT DISTINCT name, kingdom FROM "Towers") as kingdom`
-            )
-        )[0].count
+    const towerCountQueryResult = await TOWER_REPO.query(
+        `SELECT COUNT(*) FROM (SELECT DISTINCT name, kingdom FROM "Towers") as kingdom`
+    )
+    const mainStatsCountQueryResult = await MAIN_STATS_REPO.query(
+        `SELECT COUNT(DISTINCT "towerId") FROM main_stats`
     )
 
-    const mainStatsCount = Number(
-        (
-            await getRepository(MainStats, "test").query(
-                `SELECT COUNT(DISTINCT "towerId") FROM main_stats`
-            )
-        )[0].count
+    const barracksCountQueryResult = await BARRACKS_STATS_REPO.query(
+        `SELECT COUNT(DISTINCT "towerId") FROM barracks_stats`
     )
 
-    const barracksStatsCount = Number(
-        (
-            await getRepository(BarracksStats, "test").query(
-                `SELECT COUNT(DISTINCT "towerId") FROM barracks_stats`
-            )
-        )[0].count
+    const attackStatsCountQuery = await ATTACK_STATS_REPO.query(
+        `SELECT COUNT(DISTINCT "towerId") FROM attack_stats`
     )
 
-    const attackStatsCount = Number(
-        (
-            await getRepository(AttackStats, "test").query(
-                `SELECT COUNT(DISTINCT "towerId") FROM attack_stats`
-            )
-        )[0].count
-    )
+    const towerCount = Number(towerCountQueryResult[0].count)
+    const mainStatsCount = Number(mainStatsCountQueryResult[0].count)
+    const barracksStatsCount = Number(barracksCountQueryResult[0].count)
+    const attackStatsCount = Number(attackStatsCountQuery[0].count)
 
     expect(towerCount).toBe(104)
     expect(mainStatsCount).toBe(104)
@@ -161,8 +162,8 @@ test("Seed() should populate distinct rows in the tables: Towers, main_stats, ba
     // reseeding shouldn't change the rows
     // because the table wouldn't allow for duplication
     await seed({ dbName: "test", verbose: false })
-    expect(towerCount).toBe(await getRepository(Tower, "test").count())
-    expect(mainStatsCount).toBe(await getRepository(MainStats, "test").count())
-    expect(barracksStatsCount).toBe(await getRepository(BarracksStats, "test").count())
-    expect(attackStatsCount).toBe(await getRepository(AttackStats, "test").count())
+    expect(towerCount).toBe(await TOWER_REPO.count())
+    expect(mainStatsCount).toBe(await MAIN_STATS_REPO.count())
+    expect(barracksStatsCount).toBe(await BARRACKS_STATS_REPO.count())
+    expect(attackStatsCount).toBe(await ATTACK_STATS_REPO.count())
 })
