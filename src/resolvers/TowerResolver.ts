185b1083446b89166c3e6b467e53affb719e8921
Mithi <mithi.sevilla@gmail.com>
2020-10-23 05:26:03 +0800

    Remove mutations since this will be a strictly query-based project

--- src/resolvers/TowerResolver.ts
+++ src/resolvers/TowerResolver.ts
@@ -1,41 +1,10 @@
-import { Resolver, Query, Mutation, Arg, InputType, Field } from "type-graphql"
+import { Resolver, Query } from "type-graphql"
 import { Tower } from "../models/Tower"
-import { TowerType, TowerKingdom, TowerLevel } from "../enums/TowerEnums"
-
-@InputType()
-export class CreateTowerInput {
-    @Field()
-    name: string
-
-    @Field()
-    notes: string
-
-    @Field(_ => TowerType)
-    towerType: TowerType
-
-    @Field(_ => TowerLevel)
-    level: TowerLevel
-
-    @Field(_ => TowerKingdom)
-    kingdom: TowerKingdom
-}
 
 @Resolver()
 export class TowerResolver {
-    @Query(() => String)
-    hello() {
-        return "world"
-    }
-
     @Query(() => [Tower])
     towers() {
         return Tower.find()
-    }
-
-    @Mutation(() => Tower)
-    async createTower(@Arg("data") data: CreateTowerInput) {
-        const tower = Tower.create(data)
-        await tower.save()
-        return tower
     }
 }
