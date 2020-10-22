7b51066019fbc7ecc56bb7d1ae1a21cb31df78a1
Mithi <mithi.sevilla@gmail.com>
2020-10-23 05:59:52 +0800

    Query towers with pagination

--- src/resolvers/TowerResolver.ts
+++ src/resolvers/TowerResolver.ts
@@ -1,10 +1,33 @@
-import { Resolver, Query } from "type-graphql"
+import { getRepository } from "typeorm"
+import { Resolver, Query, ArgsType, Args, Field, Int } from "type-graphql"
 import { Tower } from "../models/Tower"
+
+import { Min, Max } from "class-validator"
+
+@ArgsType()
+class TowerArgs {
+    @Field(_type => Int, { defaultValue: 0 })
+    @Min(0)
+    skip: number = 0
+
+    @Field(_type => Int, { defaultValue: 104 })
+    @Min(1)
+    @Max(104)
+    take: number
+}
 
 @Resolver()
 export class TowerResolver {
     @Query(() => [Tower])
-    towers() {
-        return Tower.find()
+    async towers(@Args() { skip, take }: TowerArgs) {
+        const result: [Tower] = await getRepository(Tower).query(
+            `SELECT * FROM "Towers" ORDER BY id ASC LIMIT ${take} OFFSET ${skip}`
+        )
+
+        const cleanResult = result.map(tower => ({
+            ...tower,
+            level: Number(tower["level"]),
+        }))
+        return cleanResult
     }
 }
