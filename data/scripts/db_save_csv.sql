b795d7d16468c6e55c93c29b245f437a1e2fc544
Mithi <mithi.sevilla@gmail.com>
2020-10-24 23:07:10 +0800

    Add script to save database columns to csv and txt

---
+++ data/scripts/db_save_csv.sql
@@ -0,0 +1,21 @@
+\dt
+\dT
+\copy (SELECT * From "Towers" ORDER BY id ASC) To '../generated/csv/towers.csv' With CSV
+\copy (SELECT * From main_stats ORDER BY id ASC) To '../generated/csv/main-stats.csv' With CSV
+\copy (SELECT * From attack_stats ORDER BY id ASC) To '../generated/csv/attacks-stats.csv' With CSV
+\copy (SELECT * From barracks_stats ORDER BY id ASC) To '../generated/csv/barracks-stats.csv' With CSV
+\copy (SELECT * From ability ORDER BY id ASC) To '../generated/csv/abilities.csv' With CSV
+\copy (SELECT * From ability_level ORDER BY id ASC) To '../generated/csv/ability-levels.csv' With CSV
+
+\o ../generated/txt/towers.txt
+SELECT * FROM "Towers" ORDER BY id ASC;
+\o ../generated/txt/main-stats.txt
+SELECT * FROM main_stats ORDER BY id ASC;
+\o ../generated/txt/attack-stats.txt
+SELECT * FROM attack_stats ORDER BY id ASC;
+\o ../generated/txt/barracks-stats.txt
+SELECT * FROM barracks_stats ORDER BY id ASC;
+\o ../generated/txt/abilities.txt
+SELECT * FROM ability ORDER BY id ASC;
+\o ../generated/txt/ability-levels.txt
+SELECT * FROM ability_level ORDER BY id ASC;
