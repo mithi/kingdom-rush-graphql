970eafeec92edc66f67c360e7813937cd00e66bf
Mithi <mithi.sevilla@gmail.com>
2020-10-27 13:33:38 +0800

    Update sql scripts to included build_sequence table

--- scripts/db_save_csv.sql
+++ scripts/db_save_csv.sql
@@ -6,6 +6,7 @@
 \copy (SELECT * From barracks_stats ORDER BY id ASC) To './data/generated/csv/barracks-stats.csv' With CSV
 \copy (SELECT * From ability ORDER BY id ASC) To './data/generated/csv/abilities.csv' With CSV
 \copy (SELECT * From ability_level ORDER BY id ASC) To './data/generated/csv/ability-levels.csv' With CSV
+\copy (SELECT * From build_sequence ORDER BY id ASC) To './data/generated/csv/build-sequences.csv' With CSV
 
 \o ./data/generated/txt/towers.txt
 SELECT * FROM "Towers" ORDER BY id ASC;
@@ -19,3 +20,5 @@ SELECT * FROM barracks_stats ORDER BY id ASC;
 SELECT * FROM ability ORDER BY id ASC;
 \o ./data/generated/txt/ability-levels.txt
 SELECT * FROM ability_level ORDER BY id ASC;
+\o ./data/generated/txt/build-sequences.txt
+SELECT * FROM build_sequence ORDER BY id ASC;
--- scripts/db_table_info.sql
+++ scripts/db_table_info.sql
@@ -3,4 +3,5 @@ SELECT * FROM main_stats;
 SELECT * FROM barracks_stats;
 SELECT * FROM ability;
 SELECT * FROM ability_level;
+SELECT * FROM build_sequence;
 SELECT * FROM migrations;
