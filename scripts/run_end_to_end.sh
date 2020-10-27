b72b6644bad442dbb46552f2347d630183496b18
Mithi <mithi.sevilla@gmail.com>
2020-10-27 13:34:18 +0800

    Update package scripts

--- scripts/run_end_to_end.sh
+++ scripts/run_end_to_end.sh
@@ -1,3 +1,7 @@
+# Creates database, runs all migrations
+# Seed the db tables with the data from the csv
+# Print on the screen
+# Drop the database
 npm run db:create;
 npm run db:migrate-all;
 psql kingdom_rush_user -h localhost -d kingdom_rush_db -f ./scripts/db_load_csv.sql;
