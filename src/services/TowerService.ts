fbd89f57606c47d5d69798ffb9185800e69fbf40
Mithi <mithi.sevilla@gmail.com>
2020-10-29 17:37:21 +0800

    Add query tower by id

--- src/services/TowerService.ts
+++ src/services/TowerService.ts
@@ -7,9 +7,14 @@ import {
     BarracksTowerArgs,
     TowerArgs,
 } from "../definitions"
-import { TowerType } from "../definitions/enums"
+
+import { TowerVerbose, TowerWithNullableFields } from "../definitions/objects"
+import { TowerLevel, TowerType } from "../definitions/enums"
 import { Tower } from "../models/Tower"
 import { nothingLeft, buildQueryExpression } from "./utils"
+import { AbilityService } from "./AbilityService"
+import { BuildSequenceService } from "./BuildSequenceService"
+
 const DB_NAME = process.env.NODE_ENV === "test" ? "test" : "default"
 
 export class TowerService {
@@ -56,5 +61,74 @@ export class TowerService {
             queryExpression
         )
         return result.map(tower => ({ ...tower, level: Number(tower.level) }))
+    }
+
+    async towerById(id: Number) {
+        /*
+          get tower by id (towerWithStats)
+          if towerType is barracks get barracks stats
+          else get attack stats
+
+          populate towerWithNullableFields accordingly
+
+          if tower is level4
+          get abilities and get build sequence
+
+          update accordingly
+         */
+
+        let results = await getRepository(Tower, DB_NAME).query(
+            `SELECT * FROM "Towers" INNER JOIN main_stats ON "Towers".id = main_stats."towerId" WHERE "Towers".id = ${id}`
+        )
+
+        const basicStats: TowerWithStats = results.length !== 0 ? results[0] : null
+        if (basicStats === null) {
+            return null
+        }
+
+        let towerWithNullableFields: TowerWithNullableFields | null = null
+
+        if (basicStats.towerType === TowerType.BARRACKS) {
+            results = await getRepository(Tower, DB_NAME).query(
+                `SELECT * FROM "Towers" INNER JOIN barracks_stats ON "Towers".id = barracks_stats."towerId" WHERE "Towers".id = ${id}`
+            )
+
+            if (results.length === 0) {
+                return null
+            }
+
+            let towerStats: BarracksTower = results[0]
+            towerWithNullableFields = towerStats
+        } else {
+            results = await getRepository(Tower, DB_NAME).query(
+                `SELECT * FROM "Towers" INNER JOIN attack_stats ON "Towers".id = attack_stats."towerId" WHERE "Towers".id = ${id}`
+            )
+
+            if (results.length === 0) {
+                return null
+            }
+            let towerStats: AttackTower = results[0]
+            towerWithNullableFields = towerStats
+        }
+
+        if (Number(basicStats.level) !== TowerLevel.LVL4) {
+            const towerVerbose: TowerVerbose = {
+                allStats: towerWithNullableFields,
+            }
+
+            return towerVerbose
+        }
+
+        const abilityService = new AbilityService()
+        const abilities = await abilityService.abilitiesByTowerId(id)
+        const buildSequenceService = new BuildSequenceService()
+        const buildSequence = await buildSequenceService.buildSequenceByTowerId(id)
+
+        const towerVerbose: TowerVerbose = {
+            allStats: towerWithNullableFields,
+            abilities,
+            buildSequence,
+        }
+        return towerVerbose
     }
 }
