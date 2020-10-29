bebca7d2aa52a35bead1e614ab7168015c9b8b2d
Mithi <mithi.sevilla@gmail.com>
2020-10-29 08:02:53 +0800

    Add header to csv

--- scripts/db_save_csv.sql
+++ scripts/db_save_csv.sql
@@ -1,12 +1,12 @@
 \dt
 \dT
-\copy (SELECT * From "Towers" ORDER BY id ASC) To './data/generated/csv/towers.csv' With CSV
-\copy (SELECT * From main_stats ORDER BY id ASC) To './data/generated/csv/main-stats.csv' With CSV
-\copy (SELECT * From attack_stats ORDER BY id ASC) To './data/generated/csv/attacks-stats.csv' With CSV
-\copy (SELECT * From barracks_stats ORDER BY id ASC) To './data/generated/csv/barracks-stats.csv' With CSV
-\copy (SELECT * From ability ORDER BY id ASC) To './data/generated/csv/abilities.csv' With CSV
-\copy (SELECT * From ability_level ORDER BY id ASC) To './data/generated/csv/ability-levels.csv' With CSV
-\copy (SELECT * From build_sequence ORDER BY id ASC) To './data/generated/csv/build-sequences.csv' With CSV
+\copy (SELECT * From "Towers" ORDER BY id ASC) To './data/generated/csv/towers.csv' With CSV HEADER
+\copy (SELECT * From main_stats ORDER BY id ASC) To './data/generated/csv/main-stats.csv' With CSV HEADER
+\copy (SELECT * From attack_stats ORDER BY id ASC) To './data/generated/csv/attacks-stats.csv' With CSV HEADER
+\copy (SELECT * From barracks_stats ORDER BY id ASC) To './data/generated/csv/barracks-stats.csv' With CSV HEADER
+\copy (SELECT * From ability ORDER BY id ASC) To './data/generated/csv/abilities.csv' With CSV HEADER
+\copy (SELECT * From ability_level ORDER BY id ASC) To './data/generated/csv/ability-levels.csv' With CSV HEADER
+\copy (SELECT * From build_sequence ORDER BY id ASC) To './data/generated/csv/build-sequences.csv' With CSV HEADER
 
 \o ./data/generated/txt/towers.txt
 SELECT * FROM "Towers" ORDER BY id ASC;
