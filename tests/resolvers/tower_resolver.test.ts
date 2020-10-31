d0c07fe125a5b347e5a07216122bdac8d28a4bea
Mithi <mithi.sevilla@gmail.com>
2020-10-31 12:13:21 +0800

    Refactor duplicate code

--- tests/resolvers/tower_resolver.test.ts
+++ tests/resolvers/tower_resolver.test.ts
@@ -3,28 +3,29 @@ import { buildSchema } from "type-graphql"
 import { TowerResolver } from "../../src/resolvers/TowerResolver"
 import { ApolloServer, gql } from "apollo-server"
 import { createTestClient } from "apollo-server-testing"
-import seed from "../../src/seed"
 import ascendingTowerIds from "./__snapshots__/ASCENDING_TOWER_IDS"
 import attackTowersTypes from "./__snapshots__/ATTACK_TOWER_TYPES"
 import attackTowersFireIntervalDescending from "./__snapshots__/ATTACK_TOWER_FIRE_INTERVAL_DESCEND"
 import barracksTowers from "./__snapshots__/BARRACKS_TOWERS"
+import { DocumentNode } from "graphql"
 
 beforeAll(async () => {
-    const t0 = Date.now()
     await createConnection("test")
-    await seed({ dbName: "test", verbose: false })
-    const t1 = Date.now()
-    console.log(` 👩‍🔬 Connected and populated database in ${t1 - t0}ms`)
 })
 
 afterAll(async () => {
     await getConnection("test").close()
 })
 
-test("1. Be able to get towers, ids would be sorted in ascending order by default", async () => {
+const executeTest = async (testQuery: DocumentNode, correctAnswer: string) => {
     const schema = await buildSchema({ resolvers: [TowerResolver] })
     const { query } = createTestClient(new ApolloServer({ schema }))
 
+    const result = await query({ query: testQuery })
+    expect(result).toMatchInlineSnapshot(correctAnswer)
+}
+
+test("1. Be able to get towers, ids would be sorted in ascending order by default", async () => {
     const testQuery = gql`
         {
             towers {
@@ -32,14 +33,11 @@ test("1. Be able to get towers, ids would be sorted in ascending order by defaul
             }
         }
     `
-    const result = await query({ query: testQuery })
-    expect(result).toMatchInlineSnapshot(ascendingTowerIds())
+
+    await executeTest(testQuery, ascendingTowerIds())
 })
 
 test("2. Be able to get attack towers, by default result will be sorted by id in ascending order", async () => {
-    const schema = await buildSchema({ resolvers: [TowerResolver] })
-    const { query } = createTestClient(new ApolloServer({ schema }))
-
     const testQuery = gql`
         {
             attackTowers {
@@ -47,14 +45,10 @@ test("2. Be able to get attack towers, by default result will be sorted by id in
             }
         }
     `
-    const result = await query({ query: testQuery })
-    expect(result).toMatchInlineSnapshot(attackTowersTypes())
+    await executeTest(testQuery, attackTowersTypes())
 })
 
 test("3. Be able to get attack towers sorted by fire interval in descending order", async () => {
-    const schema = await buildSchema({ resolvers: [TowerResolver] })
-    const { query } = createTestClient(new ApolloServer({ schema }))
-
     const testQuery = gql`
         {
             attackTowers(
@@ -68,14 +62,10 @@ test("3. Be able to get attack towers sorted by fire interval in descending orde
             }
         }
     `
-    const result = await query({ query: testQuery })
-    expect(result).toMatchInlineSnapshot(attackTowersFireIntervalDescending())
+    await executeTest(testQuery, attackTowersFireIntervalDescending())
 })
 
 test("4. Be able to get barracks towers in correct order", async () => {
-    const schema = await buildSchema({ resolvers: [TowerResolver] })
-    const { query } = createTestClient(new ApolloServer({ schema }))
-
     const testQuery = gql`
         {
             barracksTowers(
@@ -91,6 +81,5 @@ test("4. Be able to get barracks towers in correct order", async () => {
             }
         }
     `
-    const result = await query({ query: testQuery })
-    expect(result).toMatchInlineSnapshot(barracksTowers())
+    await executeTest(testQuery, barracksTowers())
 })
