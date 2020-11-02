b72b3c866d6c00b4d882b534db4b499dbfc45495
Mithi <mithi.sevilla@gmail.com>
2020-11-02 17:15:55 +0800

    Add test, ability by tower

--- tests/resolvers/__CASES__/singleAbility/index.ts
+++ tests/resolvers/__CASES__/singleAbility/index.ts
@@ -1,11 +1,11 @@
 import case0 from "./case00"
 import case1 from "./case01"
 import case2 from "./case02"
-//import case3 from "./case03"
+import case3 from "./case03"
 //import case4 from "./case04"
 //import case5 from "./case05"
 //import case6 from "./case06"
 //import case7 from "./case07"
 //import case8 from "./case08"
 
-export default [case0, case1, case2]
+export default [case0, case1, case2, case3]
