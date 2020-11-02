0cf10be17188dea83a18038840bad8e16d5b022d
Mithi <mithi.sevilla@gmail.com>
2020-11-02 21:16:15 +0800

    Update barracks tower test to more closely resemble real world queries

--- tests/resolvers/__CASES__/various/case04.ts
+++ tests/resolvers/__CASES__/various/case04.ts
@@ -1,20 +1,22 @@
 import { gql } from "apollo-server"
 import { DocumentNode } from "graphql"
 
-const description = "4. Be able to get barracks...."
+const description =
+    "4. Be able to get barracks, with the more soldiers first, then sorted by kingdom, then sorted by name."
 
 const testQuery = gql`
     {
         barracksTowers(
             sortDefinition: [
-                { column: numberOfUnits, sortOrder: ASCEND }
+                { column: numberOfUnits, sortOrder: DESCEND }
                 { column: kingdom, sortOrder: DESCEND }
-                { column: name, sortOrder: DESCEND }
+                { column: name, sortOrder: ASCEND }
             ]
         ) {
             name
             kingdom
             numberOfUnits
+            towerType
         }
     }
 `
@@ -26,138 +28,165 @@ const result = () => {
         "barracksTowers": Array [
           Object {
             "kingdom": "KRV",
-            "name": "elite harassers, 4",
-            "numberOfUnits": 2,
-          },
-          Object {
-            "kingdom": "KRV",
-            "name": "elite harassers, 3",
-            "numberOfUnits": 2,
-          },
-          Object {
-            "kingdom": "KRV",
-            "name": "elite harassers, 2",
-            "numberOfUnits": 2,
-          },
-          Object {
-            "kingdom": "KRV",
-            "name": "elite harassers, 1",
-            "numberOfUnits": 2,
-          },
-          Object {
-            "kingdom": "KRV",
-            "name": "dark knights, 4",
-            "numberOfUnits": 2,
-          },
-          Object {
-            "kingdom": "KRV",
-            "name": "dark knights, 3",
-            "numberOfUnits": 2,
-          },
-          Object {
-            "kingdom": "KRV",
-            "name": "dark knights, 2",
-            "numberOfUnits": 2,
-          },
-          Object {
-            "kingdom": "KRV",
-            "name": "dark knights, 1",
-            "numberOfUnits": 2,
-          },
-          Object {
-            "kingdom": "KRO",
-            "name": "forest keepers",
-            "numberOfUnits": 2,
-          },
-          Object {
-            "kingdom": "KRV",
-            "name": "orc warriors den, 4",
+            "name": "orc warriors den, 1",
             "numberOfUnits": 3,
-          },
-          Object {
-            "kingdom": "KRV",
-            "name": "orc warriors den, 3",
-            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
             "kingdom": "KRV",
             "name": "orc warriors den, 2",
             "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
             "kingdom": "KRV",
-            "name": "orc warriors den, 1",
+            "name": "orc warriors den, 3",
             "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
-            "kingdom": "KRO",
-            "name": "warden barracks",
+            "kingdom": "KRV",
+            "name": "orc warriors den, 4",
             "numberOfUnits": 3,
-          },
-          Object {
-            "kingdom": "KRO",
-            "name": "ranger barracks",
-            "numberOfUnits": 3,
-          },
-          Object {
-            "kingdom": "KRO",
-            "name": "defender barracks",
-            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
             "kingdom": "KRO",
             "name": "bladesinger hall",
             "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
-            "kingdom": "KRF",
-            "name": "militia barracks",
+            "kingdom": "KRO",
+            "name": "defender barracks",
             "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
-            "kingdom": "KRF",
-            "name": "knights templar",
+            "kingdom": "KRO",
+            "name": "ranger barracks",
             "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
-            "kingdom": "KRF",
-            "name": "knights barracks",
+            "kingdom": "KRO",
+            "name": "warden barracks",
             "numberOfUnits": 3,
-          },
-          Object {
-            "kingdom": "KRF",
-            "name": "footmen barracks",
-            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
             "kingdom": "KRF",
             "name": "assassin's guild",
             "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
-            "kingdom": "KR",
-            "name": "militia barracks",
-            "numberOfUnits": 3,
-          },
-          Object {
-            "kingdom": "KR",
-            "name": "knights barracks",
-            "numberOfUnits": 3,
-          },
-          Object {
-            "kingdom": "KR",
-            "name": "holy order",
-            "numberOfUnits": 3,
-          },
-          Object {
-            "kingdom": "KR",
+            "kingdom": "KRF",
             "name": "footmen barracks",
             "numberOfUnits": 3,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRF",
+            "name": "knights barracks",
+            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRF",
+            "name": "knights templar",
+            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRF",
+            "name": "militia barracks",
+            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
           },
           Object {
             "kingdom": "KR",
             "name": "barbarian mead hall",
             "numberOfUnits": 3,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KR",
+            "name": "footmen barracks",
+            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KR",
+            "name": "holy order",
+            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KR",
+            "name": "knights barracks",
+            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KR",
+            "name": "militia barracks",
+            "numberOfUnits": 3,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRV",
+            "name": "dark knights, 1",
+            "numberOfUnits": 2,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRV",
+            "name": "dark knights, 2",
+            "numberOfUnits": 2,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRV",
+            "name": "dark knights, 3",
+            "numberOfUnits": 2,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRV",
+            "name": "dark knights, 4",
+            "numberOfUnits": 2,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRV",
+            "name": "elite harassers, 1",
+            "numberOfUnits": 2,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRV",
+            "name": "elite harassers, 2",
+            "numberOfUnits": 2,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRV",
+            "name": "elite harassers, 3",
+            "numberOfUnits": 2,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRV",
+            "name": "elite harassers, 4",
+            "numberOfUnits": 2,
+            "towerType": "BARRACKS",
+          },
+          Object {
+            "kingdom": "KRO",
+            "name": "forest keepers",
+            "numberOfUnits": 2,
+            "towerType": "BARRACKS",
           },
         ],
       },
