628a284e1de2d4c85d99cb599e138f08bb0056e5
Mithi <mithi.sevilla@gmail.com>
2020-11-02 17:23:37 +0800

    Add another test for abilitiesByTowerId

---
+++ tests/resolvers/__CASES__/singleAbility/case04.ts
@@ -0,0 +1,83 @@
+import { gql } from "apollo-server"
+import { DocumentNode } from "graphql"
+
+const description = "abilitiesByTowerId: When tower id exists and has abilities"
+const testQuery = gql`
+    {
+        abilitiesByTowerId(id: 9) {
+            abilityDescription
+            abilityId
+            abilityName
+            kingdom
+            levelCosts
+            numberOfLevels
+            totalAbilityCost
+            totalCostWithTowers
+            towerId
+            towerImageUrl
+            towerName
+            towerType
+        }
+    }
+`
+
+const result = () => {
+    return `
+    Object {
+      "data": Object {
+        "abilitiesByTowerId": Array [
+          Object {
+            "abilityDescription": "Sends an automated mining drill towards an enemy, reducing it to a mound of gibs. Every upgrade level decreases cooldown.",
+            "abilityId": 33,
+            "abilityName": "core drill",
+            "kingdom": "KRF",
+            "levelCosts": Array [
+              400,
+              200,
+              200,
+            ],
+            "numberOfLevels": 3,
+            "totalAbilityCost": 800,
+            "totalCostWithTowers": 1865,
+            "towerId": 9,
+            "towerImageUrl": "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krf-artillery-4b-dwaarp.png",
+            "towerName": "dwaarp",
+            "towerType": "ARTILLERY",
+          },
+          Object {
+            "abilityDescription": "Burns all enemies in close range over 4 seconds. Every upgrade level increases maximum damage and damage per second",
+            "abilityId": 34,
+            "abilityName": "furnace blast",
+            "kingdom": "KRF",
+            "levelCosts": Array [
+              300,
+              250,
+              250,
+            ],
+            "numberOfLevels": 3,
+            "totalAbilityCost": 800,
+            "totalCostWithTowers": 1865,
+            "towerId": 9,
+            "towerImageUrl": "https://storage.googleapis.com/kingdom-rush-towers.appspot.com/krf-artillery-4b-dwaarp.png",
+            "towerName": "dwaarp",
+            "towerType": "ARTILLERY",
+          },
+        ],
+      },
+      "errors": undefined,
+      "extensions": undefined,
+      "http": Object {
+        "headers": Headers {
+          Symbol(map): Object {},
+        },
+      },
+    }
+`
+}
+
+const testCase: [string, { testQuery: DocumentNode; result: Function }] = [
+    description,
+    { testQuery, result },
+]
+
+export default testCase
